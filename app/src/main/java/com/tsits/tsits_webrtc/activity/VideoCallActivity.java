05c5360a1631be03fd2ba452c8e2e4fbe85b5dd9
NIKLINT <806770109@qq.com>
2021-09-23 00:34:37 +0800

    1

---
+++ app/src/main/java/com/tsits/tsits_webrtc/activity/VideoCallActivity.java
@@ -0,0 +1,17 @@
+package com.tsits.tsits_webrtc.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tsits.tsits_webrtc.R;
+
+public class VideoCallActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_video_chat);
+
+    }
+}
